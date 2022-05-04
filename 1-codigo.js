e706a332057e7b916de7da4e40351d97ed7a3e24
gutierresleh <102757734+gutierresleh@users.noreply.github.com>
2022-05-04 16:51:03 -0300

    Add files via upload

---
+++ 1-codigo.js
@@ -0,0 +1,15 @@
+console.log('Olá, Mundo');
+console.log(7+4);
+console.log('7'+'4');
+console.log(12+20);
+console.log(25+157);
+console.log(30+16);
+console.log('Leticia' + 'Lorena');
+
+const nome ='Leticia';
+console.log(nome)
+
+const a = 1500;
+const b = 1200;
+
+console.log(a+b);
---
+++ exercícios.js
@@ -0,0 +1,43 @@
+//1-a
+
+console.log(10+20*30);
+
+//1-b
+
+console.log(4**2/30);
+
+//1-c
+
+console.log((9**4 + 2)* 6 - 1);
+
+//2
+
+console.log(10%3*10**1+1-10*4/2);
+
+//3
+
+const nome ='Leticia';
+console.log(nome)
+
+//4
+
+const x = 2;
+const y = 3;
+
+console.log(x+y);
+
+//5
+
+const l = 8;
+const i = 3;
+const f = 5;
+
+console.log(l+y+f);
+
+//6
+
+const h = 1800
+console.log(h*2);
+
+
+
